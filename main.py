eb9c9481417bbeb02f77e0242fe4db4f02f290de
ValentinKranz <valentin.kranz@gmail.com>
2025-11-20 19:54:42 -0300

    add moving left and right method

--- main.py
+++ main.py
@@ -17,13 +17,20 @@ Screen height: {SCREEN_HEIGHT}""")
     
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
- 
+
+
 if __name__ == "__main__":
     main()
