dc03f5c7ea7868fffd472fe351455c62207767d2
ValentinKranz <valentin.kranz@gmail.com>
2025-11-26 21:28:26 -0300

    asteroid shape improved

--- asteroids.py
+++ asteroids.py
@@ -1,15 +1,32 @@
 import pygame
 import random
+import math
 from circleshape import CircleShape
-from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_IRREGULARITY, ASTEROID_POINTS
 from logger import  log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-    
+        self.points = self.generate_polygon()
+
+    def generate_polygon(self, irregularity = ASTEROID_IRREGULARITY, points = ASTEROID_POINTS):
+        shape = []
+        angle_step = 360 / points
+        
+        for i in range(points):
+            angle = math.radians(i * angle_step)
+            offset = random.uniform(1 - irregularity, 1 + irregularity)
+            r = self.radius * offset
+            x = math.cos(angle) * r
+            y = math.sin(angle) * r
+            shape.append((x, y))
+
+        return shape
+
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        poly = [(self.position.x + px, self.position.y + py) for px, py in self.points]
+        pygame.draw.polygon(screen, "white", poly, LINE_WIDTH)
     
     def update(self, dt):
         self.position += self.velocity * dt
